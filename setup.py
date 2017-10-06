cde0ddbd200a5a716b2b2df7d5d3d2ba1adf323d
Paul Yuan <puyuan1@gmail.com>
2017-10-06 15:34:47 +0800

    Update metadata

--- setup.py
+++ setup.py
@@ -2,12 +2,15 @@ from setuptools import setup, find_packages
 
 setup(
     name='photodedup',
-    version='0.2',
-    description='A simple photo deduplicator tool written in Python',
+    version='0.2.1',
+    description='A simple photo deduplication tool written in Python',
     author='Paul Yuan',
     author_email='puyuan1@gmail.com',
-    url='https://github.com/puyuan',
-    install_requires=['markdown'],
+    url='https://github.com/puyuan/photodedup',
+    setup_requires=['setuptools-markdown'],
+    long_description_markdown_filename='README.md',
+    install_requires=['exifread'],
+    keywords="photo dedup deduplication duplicate exif",
     entry_points={'console_scripts' : ['photodedup=photodedup:main']},
     packages=find_packages()
 )
